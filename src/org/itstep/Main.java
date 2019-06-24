8e5e7998427b4bf2c8431e1447a47ffd96eb8150
Smakovskaya <anna318420>
2019-06-24 16:39:40 +0300

    Initial commit

--- src/org/itstep/Main.java
+++ src/org/itstep/Main.java
@@ -1,29 +1,20 @@
 package org.itstep;
 
 public class Main {
-   public static void main(String[] args) {
-       //  int k = 12345; int x = k;
-       //  int s = 0;
-       //   while (x != 0 ){
-       //       s += x%10;
-       //        x /= 10;
-       //    }
-       //        System.out.println("Сумма цифр в числе " + k + " = " + s);
+    public static void main(String[] args) {
+        int number = 12345;
+        int sum = 0;        // обьявляем переменную и присваиваем ей значение 0
+        int a = 0;
+        String s = String.valueOf(number);  // переменную number преобразуем в строку
+        int numbers = s.length();           // находим длинну этой строки
+        for (int i = 0; i < numbers; i++) {   // цикл длится столько, сколько символов в строке
+            a = Integer.parseInt(Character.toString(s.charAt(i)));  //Получаем Итый символ и преобразуем его в тип Integer
+            sum = a + sum;   // Каждый цикл к полученной цифре прибавляем сумму
+        }
 
-
-       int number = 12345;
-       int sum = 0;        // обьявляем переменную и присваиваем ей значение 0
-       int a =0;
-       String s = String.valueOf(number);  // переменную number преобразуем в строку
-       int numbers = s.length();           // находим длинну этой строки
-       for(int i = 0; i < numbers; i++){   // цикл длится столько, сколько символов в строке
-           a = Integer.parseInt(Character.toString(s.charAt(i)));  //Получаем Итый символ и преобразуем его в тип Integer
-           sum = a + sum;   // Каждый цикл к полученной цифре прибавляем сумму
-       }
-
-       System.out.println(sum);
-   }
-   }
+        System.out.println(sum);
+    }
+}
 
 
 
---
+++ src/org/itstep/Main2.java
@@ -0,0 +1,14 @@
+package org.itstep;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int k = 12345;
+        int x = k;
+        int s = 0;
+        while (x != 0) {
+            s += x % 10;
+            x /= 10;
+        }
+        System.out.println("Сумма цифр в числе " + k + " = " + s);
+    }
+}
