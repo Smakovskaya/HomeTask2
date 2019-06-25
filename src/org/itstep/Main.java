9a95439713492072038b39c9b760796847e70709
Smakovskaya <anna318420>
2019-06-25 13:14:22 +0300

    Fixed

--- src/org/itstep/Main.java
+++ src/org/itstep/Main.java
@@ -2,19 +2,31 @@ package org.itstep;
 
 public class Main {
     public static void main(String[] args) {
-        int number = 12345;
-        int sum = 0;        // обьявляем переменную и присваиваем ей значение 0
-        int a = 0;
-        String s = String.valueOf(number);  // переменную number преобразуем в строку
-        int numbers = s.length();           // находим длинну этой строки
-        for (int i = 0; i < numbers; i++) {   // цикл длится столько, сколько символов в строке
-            a = Integer.parseInt(Character.toString(s.charAt(i)));  //Получаем Итый символ и преобразуем его в тип Integer
-            sum = a + sum;   // Каждый цикл к полученной цифре прибавляем сумму
+        int resultByDiv = getSumByDiv(12345);
+        System.out.println(resultByDiv);
+
+        int resultSyString = getSumByString(12345);
+        System.out.println(resultSyString);
+    }
+
+    private static int getSumByString(int number) {
+        int result = 0;
+        String numberAsString = String.valueOf(number);
+        char[] symbols = numberAsString.toCharArray();
+
+        for (char symbol : symbols){
+            result += Character.getNumericValue(symbol);
+        }
+        return result;
+    }
+
+    private static int getSumByDiv(int number) {
+        int result = 0;
+        while (number !=0){
+            result += number % 10;
+            number = number / 10;
         }
 
-        System.out.println(sum);
+        return result;
     }
 }
-
-
-
